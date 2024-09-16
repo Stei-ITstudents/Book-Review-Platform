495686ad86df7bbeb860d072dfc1766f646b48e6
CristyNel <nelly_4te@yahoo.it>
2024-09-16 01:59:11 +0100

    Create booknook database and seed initial data

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,4 +1,8 @@
--- book table
+-- create booknook batabase if doesn't exist
+CREATE DATABASE IF NOT EXISTS booknook;
+-- use booknook database
+USE booknook;
+-- books table
 CREATE TABLE IF NOT EXISTS books (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -21,16 +25,20 @@ CREATE TABLE IF NOT EXISTS reviews (
 );
 
 -- initial data
-INSERT INTO books (title, author, published_date, genre, summary)
+INSERT IGNORE INTO books (title, author, published_date, genre, summary)
 VALUES
-    ('The Great Gatsby', 'F. Scott Fitzgerald', '1925-04-10', 'Fiction', 'A novel set in the Roaring Twenties.'),
-    ('To Kill a Mockingbird', 'Harper Lee', '1960-07-11', 'Fiction', 'A story of racial injustice in the Deep South.'),
-    ('1984', 'George Orwell', '1949-06-08', 'Dystopian', 'A dystopian social science fiction novel.');
+    ('100 Page Python Intro: Beyond the Basic Stuff with Python', 'Sundeep Agarwal', '2019-01-01', 'Programming, Python', 'A concise introduction to Python programming.'),
+    ('JavaScript Notes for Professionals', 'Stack Overflow Community', '2021-01-01', 'Programming, JavaScript', 'A comprehensive reference guide to JavaScript.'),
+    ('Essential Node.js', 'Kevin Skoglund', '2016-01-01', 'Programming, Node.js', 'A practical introduction to Node.js.'),
+    ('Adaptive Web Design', 'Ethan Marcotte', '2010-01-01', 'Web Design, Responsive Design', 'A guide to creating websites that adapt to different screen sizes.')
+;
 
 -- Insert values into reviews
 INSERT INTO reviews (book_id, user_id, rating, review)
 VALUES
-    (1, 'user123', 5, 'An amazing read with deep themes.'),
-    (2, 'user456', 4, 'A powerful novel with strong characters.'),
-    (3, 'user789', 5, 'A chilling portrayal of a dystopian future.');
-    
+    ((SELECT id FROM books WHERE title = '100 Page Python Intro: Beyond the Basic Stuff with Python' LIMIT 1), 1, 5, 'A concise and well-explained guide to Python for beginners.'),
+    ((SELECT id FROM books WHERE title = 'JavaScript Notes for Professionals' LIMIT 1), 2, 4, 'Covers advanced topics in JavaScript, ideal for intermediate learners.'),
+    ((SELECT id FROM books WHERE title = 'Essential Node.js' LIMIT 1), 3, 5, 'A focused guide to Node.js covering core concepts and practical applications.'),
+    ((SELECT id FROM books WHERE title = 'Adaptive Web Design' LIMIT 1), 4, 4, 'A practical guide to creating responsive web designs that adapt to different screen sizes.'),
+    ((SELECT id FROM books WHERE title = 'Adaptive Web Design' LIMIT 1), 5, 5, 'Explores adaptive design techniques for creating accessible websites, with insights for enhancing user experience across devices.')
+;
